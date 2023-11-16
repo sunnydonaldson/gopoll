3a4ccd335be4860781515677fa90d8b58c089951
Sunny <54858282+sunnydonaldson@users.noreply.github.com>
2023-11-16 19:02:35 +0000

    Add support for auto installing deps on mac, and linux systems with apt.
    Also sets the directory to watch to be where the command is executed from, not just a hardcoded directory as was.

--- gopoll.sh
+++ gopoll.sh
@@ -3,5 +3,26 @@
 # A bash script for running go tests for a package on file change.
 # Runs all tests recursively on startup, and all the tests for a file's package on change.
 
+# Check deps
+uninstalled=()
+if [[ ! command -v go &> /dev/null ]]; then
+  $uninstalled += "go"
+if [[! command -v fswatch &> /dev/null ]]; then
+  $uninstalled += "fswatch"
+fi
+
+if [[ uname -eq "Darwin"]]; then
+  if [[ ! command -v brew &> /dev/null ]]; then
+    echo >&2 "Must install Homebrew"
+    exit 1
+  fi
+  brew install ${uninstalled[@]}
+else [[ ! command -v apt &> /dev/null ]]
+  if [[ ! command -v apt &> /dev/null ]]; then
+    echo >&2 "Gopoll only supports auto install for systems with apt. Please install deps manually."
+  fi
+  apt install ${uninstalled[@]}
+fi
+
 go test -v ./...
-fswatch -r0 ~/Documents/projects/heimdall | xargs -0 -I {} dirname {} | xargs -I {} go test -v {}
+fswatch -r0 . | xargs -0 -I {} dirname {} | xargs -I {} go test -v {}
